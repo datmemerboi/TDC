b43e96db670755b2f654c4d166873783aa1f9b1a
datmemerboi <shravanssd@gmail.com>
2020-04-01 14:12:47 +0530

    Multiple columns added

--- static/js/view.js
+++ static/js/view.js
@@ -1,18 +1,31 @@
 window.onload= ()=>{
   $.post('http://localhost:9090/showdata', {},(data)=>{
-      data.forEach((value, index) => {
+      data.forEach((record, index) => {
         var row = document.createElement("TR");
 
         var name = document.createElement('TD');
-        name.innerHTML = value.Name;
+        name.innerHTML = record.Name;
         var age = document.createElement('TD');
-        age.innerHTML = value.Age;
+        age.innerHTML = record.Age;
         var sex = document.createElement('TD');
-        sex.innerHTML = value.Sex;
+        sex.innerHTML = record.Sex;
+        var treatmentDate = document.createElement('TD');
+        treatmentDate.innerHTML = record['Treatment-Date'];
+        var medHist = document.createElement('TD');
+        medHist.innerHTML = record['Med-History'];
+        var treatement = document.createElement('TD');
+        treatement.innerHTML = record.Treatment;
+        var currentMeds = document.createElement('TD');
+        currentMeds.innerHTML = record['Current-Meds'];
+        var nextTreatment = document.createElement('TD');
+        nextTreatment.innerHTML = record['Next-Treatment'];
 
         row.appendChild(name); row.appendChild(age); row.appendChild(sex);
+        row.appendChild(treatmentDate);row.appendChild(medHist);row.appendChild(treatement);
+        row.appendChild(currentMeds);row.appendChild(nextTreatment);
 
         document.getElementById('patient-table').appendChild(row);
       });
+      
  })
 };
