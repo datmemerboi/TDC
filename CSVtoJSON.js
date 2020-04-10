ba4872171b28560598090f17571c2c2c53d4da36
datmemerboi <shravanssd@gmail.com>
2020-04-10 16:13:31 +0530

    Conversion using neat-csv

--- CSVtoJSON.js
+++ CSVtoJSON.js
@@ -8,7 +8,7 @@ fs.readFile( path.join(__dirname, "/data/data.csv"), async (err, data)=>{
   fs.writeFile( path.join(__dirname, "/data/data.json"), JSON.stringify(await neatcsv(data) ), (err)=>{
     if(err){ throw err  }
     else {
-      console.log("Done! Go check /data/data.json");
+      console.log("--- /data/data.csv written into /data/data.json ---");
     }
   } );
 });
