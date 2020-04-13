71c2f9409babb5da8014df800aa0fb57d0bb51fc
datmemerboi <shravanssd@gmail.com>
2020-04-13 17:06:56 +0530

    Removed console logs

--- CSVtoJSON.js
+++ CSVtoJSON.js
@@ -4,7 +4,6 @@ const neatcsv = require('neat-csv');
 
 fs.readFile( path.join(__dirname, "/data/data.csv"), async (err, data)=>{
   if(err){  throw err }
-  console.log( await neatcsv(data) );
   fs.writeFile( path.join(__dirname, "/data/data.json"), JSON.stringify(await neatcsv(data) ), (err)=>{
     if(err){ throw err  }
     else {
