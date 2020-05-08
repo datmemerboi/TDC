5a43035ee65b33eda5308f11a534631dfa721b1a
datmemerboi <shravanssd@gmail.com>
2020-05-08 21:02:51 +0530

    Working with arguments

--- JSONtoCSV.js
+++ JSONtoCSV.js
@@ -1,7 +1,20 @@
 const fs = require('fs');
 const path = require('path');
+const dateformat = require('./date-format-module.js');
 
-fs.readFile( path.join(__dirname, "/data/data.json"), async (err, json)=>{
+if( process.argv[2] ) {
+  fromFile = process.argv[2].replace("data/",'');
+  toFile = fromFile.replace('.json', '.csv');
+} else { fromFile = "data.json"; toFile = "data.csv"; }
+
+if( process.argv[3] ){
+  toFile = process.argv[3].replace("data/",'');
+}
+
+console.log( fromFile, toFile );
+
+
+fs.readFile( path.join(__dirname, "data", fromFile), (err, json)=>{
   if(err){  throw err }
   json = JSON.parse(json);
 
@@ -13,25 +26,30 @@ fs.readFile( path.join(__dirname, "/data/data.json"), async (err, json)=>{
       columns = columns + "," + item;
   });
   columns+="\n";
-  fs.writeFile( path.join(__dirname, "/data/data2.csv"), columns, (err)=>{
+
+  fs.writeFile( path.join(__dirname, "data", toFile), columns, (err)=>{
       if(err) { throw err }
     });
 
   json.forEach( (record)=>{
+
     var line = record.Name+","+
+    record['Phone No']+","+
     record.Age+","+
     record.Sex+","+
-    record['Treatment-Date']+","+
-    record['Med-History']+","+
+    dateformat.ddmonyyyy(record['Treatment Date'])+","+
+    record['Provisional Diagnosis']+","+
+    record.Investigations+","+
+    record['Final Diagnosis']+","+
     record.Treatment+","+
-    record['Current-Meds']+","+
-    record['Next-Treatment']+"\n";
+    record.Result+","+
+    dateformat.ddmonyyyy(record['Next Appointment'])+","+
+    record['Additional Information']+","+
+    record['Inital of the Medical Officer']+"\n";
 
-    fs.appendFile( path.join(__dirname, "/data/data2.csv"), line, (err)=>{
+    fs.appendFile( path.join(__dirname, "data", toFile), line, (err)=>{
       if(err) { throw err }
-      else {
-        console.log("--- /data/data.json written into /data/data2.csv ---");
-      }
     });
   });
+  console.log("--- /data/"+fromFile+" written into /data/"+toFile+" ---");
 });
