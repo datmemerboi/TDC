2d6001a1a5498052fc63e72b1347640576422b08
datmemerboi <shravanssd@gmail.com>
2020-06-07 21:25:24 +0530

    Setup for windows

--- win-setup.bat
+++ win-setup.bat
@@ -1,7 +1,6 @@
 @echo off
 echo ---
 
-
 echo @echo off > C:\Users\%USERNAME%\Desktop\RunTDC.bat
 echo @title="TDC Server" >> C:\Users\%USERNAME%\Desktop\RunTDC.bat
 echo echo Starting TDC.. >> C:\Users\%USERNAME%\Desktop\RunTDC.bat
