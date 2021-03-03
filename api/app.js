29e2f8d5b7ce4be8e50499d6a516307805f83394
datmemerboi <shravanssd@gmail.com>
2021-03-03 23:58:17 +0530

    API entry point

---
+++ api/app.js
@@ -0,0 +1,34 @@
+const express = require('express'),
+      app = express(),
+      AppointmentRoutes = require('./routes/appointment-routes'),
+      TreatmentRoutes = require('./routes/treatment-routes'),
+      PatientRoutes = require('./routes/patient-routes');
+
+const config = require('./config.json')[process.env.NODE_ENV ?? "dev"],
+      PORT = config?.PORT ?? 8080;
+
+app.use(express.json());
+
+app.use('/api/patient', PatientRoutes);
+app.use('/api/treatment', TreatmentRoutes);
+app.use('/api/appointment', AppointmentRoutes);
+
+app.get('/_health', (req, res) => {
+  console.log(`[API] ${req.method} request to /_health/`);
+  console.log(`[API] Request handled successfully`);
+  res.json({ success: true, health: "Good" }).end();
+});
+
+app.all('*', (req, res) => {
+  res.sendStatus(404).end();
+})
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`API running at http://localhost:${PORT}\nOpen a browser and go to http://localhost:${PORT}/_health/\n`);
+  }
+});
+
+module.exports = app;
