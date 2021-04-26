b8ab012b3af281b4e9c59f42beb7eb9fbd15592c
datmemerboi <shravanssd@gmail.com>
2021-04-26 23:59:23 +0530

    Add test cases

---
+++ test/index.test.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const http = require('chai-http');
+const app = require('../api/app.js');
+
+chai.use(http);
+var expect = chai.expect;
+
+describe("GET /_health", () => {
+  it("Check health of entire app", (done) => {
+    chai.request(app)
+      .get('/_health')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property("success");
+        expect(res.body.success).to.be.true;
+        done();
+      });
+  })
+});
---
+++ test/treatment.test.js
@@ -0,0 +1,137 @@
+const chai = require('chai');
+const http = require('chai-http');
+const { toPlainObject } = require('lodash');
+const app = require('../api/app.js');
+const dbUtils = require('../api/utils/db-utils');
+
+chai.use(http);
+let expect = chai.expect;
+
+describe("Treatment test cases", () => {
+  before(async function () {
+    const db = await dbUtils.connect();
+    await db.Treatment.deleteMany({});
+  });
+  after(async function () {
+    const db = await dbUtils.connect();
+    await db.Treatment.deleteMany({});
+    await dbUtils.close();
+  })
+  describe("POST /api/treatment/new", () => {
+    it("Create a new treatment", (done) => {
+      let treatmentObj = {
+        p_id: "PAT0001",
+        procedure_done: "Filling",
+        teeth_number: "31,32,33",
+        treatment_date: new Date("2021-07-13T19:00:00Z"),
+        doctor: "Dr. Zeo Test"
+      };
+      chai.request(app)
+        .post('/api/treatment/new')
+        .send(treatmentObj)
+        .end((err, res) => {
+          expect(res).to.have.status(201);
+          expect(res.body).to.be.an("object");
+          expect(res.body).to.have.property("t_id");
+          expect(res.body.teeth_number).to.be.an("array");
+          expect(res.body.teeth_number).to.have.lengthOf(3);
+          done();
+        });
+    });
+  });
+  describe("GET /api/treatment/all", () => {
+    it("List all treatments", (done) => {
+      chai.request(app)
+        .get('/api/treatment/all')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an("object");
+          expect(res.body).to.have.property("docs");
+          expect(res.body.docs).to.be.an("array");
+          expect(res.body.docs.length).to.be.greaterThan(0);
+          done();
+        });
+    });
+  });
+  describe("GET /api/treatment/get", () => {
+    it("Get particular treatment", (done) => {
+      chai.request(app)
+        .get('/api/treatment/get/TRT0001')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an("object");
+          expect(res.body).to.have.property("p_id");
+          expect(res.body).to.have.property("t_id");
+          expect(res.body).to.have.property("created_at");
+          expect(res.body.t_id).to.eql("TRT0001");
+          done();
+        });
+    });
+  });
+  describe("PUT /api/treatment/update", () => {
+    it("Update treatment object", (done) => {
+      let treatmentObj = {
+        p_id: "PAT0001",
+        treatment_date: new Date("2021-01-13"),
+        doctor: "Dr. Pro Test"
+      };
+      chai.request(app)
+        .put('/api/treatment/update/TRT0001')
+        .send(treatmentObj)
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an("object");
+          expect(res.body).to.have.property("p_id");
+          expect(res.body).to.have.property("doctor");
+          expect(res.body.doctor).to.eql(treatmentObj.doctor);
+          done();
+        });
+    });
+  });
+  describe("GET /api/treatment/patient", () => {
+    it("Get treatments for a patient", (done) => {
+      chai.request(app)
+        .get('/api/treatment/patient/PAT0001')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an("object");
+          expect(res.body).to.have.property("docs");
+          expect(res.body.docs).to.be.an("array");
+          expect(res.body.docs.length).to.be.greaterThan(0);
+          done();
+        });
+    });
+  });
+  describe("GET /api/treatment/history", () => {
+    it("Get patient's treatment history", (done) => {
+      chai.request(app)
+        .get('/api/treatment/history/PAT0001')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an("object");
+          expect(res.body).to.have.property("procedures");
+          expect(res.body).to.have.property("last_visit");
+          expect(res.body).to.have.property("doctors");
+          expect(res.body.procedures).to.be.an("array");
+          expect(res.body.procedures.length).to.be.greaterThan(0);
+          expect(res.body.doctors).to.be.an("array");
+          expect(res.body.doctors.length).to.be.greaterThan(0);
+          done();
+        });
+    });
+  });
+  describe("GET /api/treatment/doctor", () => {
+    it("Get list of treatments by a doctor", (done) => {
+      chai.request(app)
+        .get('/api/treatment/doctor?doctor=Dr.%20Pro%20Test')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an("object");
+          expect(res.body).to.have.property("docs");
+          expect(res.body.docs).to.be.an("array");
+          expect(res.body.docs.length).to.be.greaterThan(0);
+          done();
+        });
+    });
+  });
+});
